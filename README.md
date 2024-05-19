fcf72717bedf21cb0f44a0eea3ec36f44b0507ea
ning08 <gych5647@naver.com>
2024-05-20 01:45:09 +0900

    price 제외 최종본

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# back-end
+price 데이터만 create문이 아니라 import로 삽입할 것 <br/>
+추가로 price 데이터는 자영업자가 데이터를 입력해야될 부분이 있어 일부 비어있음
--- table.sql
+++ table.sql
@@ -1,6 +1,9 @@
+use capstone;
+ALTER TABLE member_table AUTO_INCREMENT=1;
+
 
 create table user_info(
-	u_id VARCHAR(255) not null,
+	u_id bigint not null,
 	nickname VARCHAR(50),
 	pwd VARCHAR (255) not null,
 	userimg_path VARCHAR(255),
@@ -9,8 +12,8 @@ create table user_info(
 );
 
 create table community(
-	c_id VARCHAR(255) not null,
-    u_id VARCHAR(255) not null,
+	c_id bigint not null,
+    u_id bigint not null,
     c_title VARCHAR(50) not null,
     viewCnt INT default 0,
     writer VARCHAR(20) not null,
@@ -22,9 +25,9 @@ create table community(
 );
 
 create table comments(
-	com_id VARCHAR(255) not null,
-    u_id VARCHAR(255) not null,
-    c_id VARCHAR(255) not null,
+	com_id bigint not null,
+    u_id bigint not null,
+    c_id bigint not null,
     contents VARCHAR(1000) not null,
     write_time timestamp not null,
     primary key(com_id),
@@ -33,19 +36,19 @@ create table comments(
 );
 
 create table diet_list(
-	dl_id VARCHAR(255) not null auto_increment,
-    u_id VARCHAR(255) not null,
-    d_id VARCHAR(255) not null,
+	dl_id bigint not null auto_increment,
+    u_id bigint not null,
+    d_id bigint not null,
     r_title VARCHAR(50) not null,
     write_time timestamp,
-    primary key(l_id),
+    primary key(dl_id),
     foreign key(u_id) references user_info(u_id),
     foreign key(d_id) references diet(d_id)
 );
 
 create table diet(
-	d_id VARCHAR(255) not null,
-    r_id VARCHAR(255) not null,
+	d_id bigint not null,
+    r_id bigint not null,
     rice VARCHAR(50) not null,
     soup VARCHAR(50) not null,
     main VARCHAR(50) not null,
@@ -56,23 +59,21 @@ create table diet(
 );
 
 create table recipe(
-	r_id VARCHAR(255) not null,
-    d_id VARCHAR(255) not null,
+	r_id bigint not null,
     title VARCHAR(50) not null,
     category VARCHAR(255) not null,
     eng VARCHAR(20) not null,
-    cal VARCHAR(20) not null,
-    por VARCHAR(20) not null,
+    car VARCHAR(20) not null,
+    pro VARCHAR(20) not null,
     fat VARCHAR(20) not null,
     na VARCHAR(20) not null,
     image VARCHAR(255),
-    PRIMARY KEY(r_id),
-    FOREIGN KEY(d_id) REFERENCES diet_list(dl_id)
+    PRIMARY KEY(r_id)
 );
 
 create table step(
-	s_id VARCHAR(255) not null,
-    r_id VARCHAR(255) not null,
+	s_id bigint not null,
+    r_id bigint not null,
     manual01 VARCHAR(1000),
     manual02 VARCHAR(1000),
     manual03 VARCHAR(1000),
@@ -84,20 +85,28 @@ create table step(
 );
 
 create table ingredients(
-	ing_id VARCHAR(255) not null,
-    r_id VARCHAR(255) not null,
+	ing_id bigint not null,
+    r_id bigint not null,
     ing_name VARCHAR(50) not null,
-    ing_amount VARCHAR(50) not null,
+    ing_amount bigint null,
     PRIMARY KEY(ing_id),
     FOREIGN KEY(r_id) REFERENCES recipe(r_id)
 );
 
 create table price(
-	ing_id VARCHAR(255) not null,
-    p_id VARCHAR(255) not null,
-	today VARCHAR(50),
-	r_price VARCHAR(50),
-	w_price VARCHAR(50),
-    PRIMARY KEY(p_id),
-    FOREIGN KEY(ing_id) REFERENCES ingredients(ing_id)
+    p_id bigint not null,
+    product_name VARCHAR(50),
+	price VARCHAR(50),
+    price_gram VARCHAR(50),
+    PRIMARY KEY(p_id)
 );
+
+create table member(
+id int,
+member_email VARCHAR(40),
+member_name VARCHAR(20),
+member_password VARCHAR(40),
+primary key(id)
+);
+
+
